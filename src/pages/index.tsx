1547323160e170ce67b4c3340986719c6f44326f
Michael Kargl <michael.kargl@zuehlke.com>
2025-05-02 08:36:46 +0200

    feat: adds Thats Me view with monitor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,36 @@
 import * as React from "react"
 import type {HeadFC, PageProps} from "gatsby"
 import {Layout} from "../components";
+import {Button, Frame, Monitor, Toolbar, Window, WindowContent, WindowHeader} from "react95";
 
 const IndexPage: React.FC<PageProps> = () => {
     return (
         <div className='index-component'>
             <Layout>
-                <span>Pie halvah shortbread halvah bear claw. Dessert lemon drops soufflé chocolate cake halvah wafer dragée. Sesame snaps oat cake sesame snaps dessert cheesecake muffin cake liquorice topping. Gummi bears chocolate topping macaroon bonbon bonbon marshmallow lemon drops. Bear claw sugar plum powder pastry tootsie roll muffin. Pie muffin ice cream tiramisu cupcake dragée sugar plum. Gummies pastry cookie biscuit cupcake croissant candy dragée. Liquorice gummi bears sweet roll oat cake lollipop. Sesame snaps pie pastry jelly-o caramels bear claw carrot cake. Marzipan sweet dessert chocolate bar marshmallow lemon drops. Sweet roll carrot cake jelly gingerbread jelly beans apple pie tart oat cake caramels. Soufflé apple pie cheesecake soufflé topping powder soufflé gummies tart. Dessert cotton candy topping jelly-o sugar plum dessert. Croissant chocolate cake icing carrot cake cookie</span>
-                <span></span>
+                <Window style={{position: 'relative'}}>
+                    <WindowHeader >
+                        <span>Thats Me!</span>
+                    </WindowHeader>
+                    <Toolbar>
+                        <Button variant='menu' size='sm' disabled>
+                            File
+                        </Button>
+                        <Button variant='menu' size='sm' disabled>
+                            Edit
+                        </Button>
+                        <Button variant='menu' size='sm' disabled>
+                            Save
+                        </Button>
+                    </Toolbar>
+                    <WindowContent>
+                        <Monitor backgroundStyles={{ backgroundImage: 'url("./avatar.png")', backgroundSize: '80%'}}></Monitor>
+                        <p>
+                            When you set &quot;resizable&quot; prop, there will be drag handle
+                            in the bottom right corner (but resizing itself must be handled by
+                            you tho!)
+                        </p>
+                    </WindowContent>
+                </Window>
             </Layout>
         </div>
     )
